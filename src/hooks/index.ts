30933737c4ca526323a86f3afdc0179715d7921d
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2023-08-11 13:03:43 +0600

    fix: gnosis connect hooks sync

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -17,11 +17,10 @@ export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & 
 export function useEagerConnect() {
   const { activate, active } = useWeb3ReactCore() // specifically using useWeb3ReactCore because of what this hook does
   const [tried, setTried] = useState(false)
-  debugger
-  useSafeAppConnection(safe)
+  const safeConnectionTried = useSafeAppConnection(safe)
 
   useEffect(() => {
-    if (!active) {
+    if (safeConnectionTried && !active) {
       injected.isAuthorized().then(isAuthorized => {
         if (isAuthorized) {
           activate(injected, undefined, true).catch(() => {
@@ -40,7 +39,7 @@ export function useEagerConnect() {
     } else {
       setTried(true)
     }
-  }, [activate]) // intentionally only running on mount (make sure it's only mounted once :))
+  }, [activate, safeConnectionTried]) // intentionally only running on mount (make sure it's only mounted once :))
 
   // if the connection worked, wait until we get confirmation of that to flip the flag
   useEffect(() => {
